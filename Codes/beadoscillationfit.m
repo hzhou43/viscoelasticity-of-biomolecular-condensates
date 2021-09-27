c9906e1e37e04fb7a449a1a8d39ead22aabf76ce
hzhou43 <87381426+hzhou43@users.noreply.github.com>
2021-09-27 08:28:35 -0500

    Update and rename oscillationfit.m to beadoscillationfit.m

--- Codes/beadoscillationfit.m
+++ Codes/beadoscillationfit.m
@@ -1,7 +1,7 @@
-function [phasediff,output] = oscillationfit(Filename,initforce,initamp,frequency)
+function output = beadoscillationfit(Filename,initforce,initamp,frequency,r,Kt)
 % Input the filename (e.g. 'forcefile.h5'), an initial value for the force
-% signal, an initial value for the amplitude and the frequency of
-% oscillation.
+% signal, an initial value for the amplitude, the frequency of
+% oscillation, the bead radius r in um, and the trap stiffness Kt in pN/um.
 
 Force1x = h5read(Filename,'/Force HF/Force 1x');
 
@@ -13,7 +13,7 @@ timeindex = [0:1:length(Force1x)-1];
 timeindex = timeindex';
 t = timeindex./78125;
 
-% Both the x component of force signal from trap 1 and the trap 1 position are fit to a sine function.
+% Both the x component of force signal from trap 1 and the trap 1 position are fit to a cosine function.
 options1 = fitoptions('Method','NonlinearLeastSquares',...
                'Lower',[-Inf,-Inf,-Inf,0],...
                'Upper',[Inf,Inf,Inf,2*pi],...
@@ -24,15 +24,15 @@ options1 = fitoptions('Method','NonlinearLeastSquares',...
 options2 = fitoptions('Method','NonlinearLeastSquares',...
                'Lower',[0,0,-Inf,0],...
                'Upper',[Inf,Inf,Inf,2*pi],...
-               'Startpoint',[4.2 0.125 freq pi],'algorithm','Trust-Region',...
+               'Startpoint',[4.2 0.125 frequency pi],'algorithm','Trust-Region',...
                'DiffMinChange',(1.0E-8),'DiffMaxChange',(0.1),'MaxFunEvals',(600),'MaxIter',(400),...
                'TolFun',(1.0E-6),'TolX',(1.0E-6));
            
-f1 = fittype('a0 + abs(a).*sin(2*pi.*f1.*t + abs(phi1))',...
+f1 = fittype('a0 + abs(a).*cos(2*pi.*f1.*t + abs(phi1))',...
     'dependent',{'Force1x'},'independent',{'t'},...
     'coefficients',{'a0','a','f1','phi1'});
 
-f2 = fittype('b0 + abs(b).*sin(2*pi.*f2.*t + abs(phi2))',...
+f2 = fittype('b0 + abs(b).*cos(2*pi.*f2.*t + abs(phi2))',...
     'dependent',{'trapposition'},'independent',{'t'},...
     'coefficients',{'b0','b','f2','phi2'});
 
@@ -43,7 +43,7 @@ f1 = c1.f1;
 phi1 = c1.phi1;
 a = abs(a);
 phi1 = abs(phi1);
-Force1xfit = a0 + a.*sin(2*pi.*f1.*t + phi1);
+Force1xfit = a0 + a.*cos(2*pi.*f1.*t + phi1);
 
 [c2,gof2,output2] = fit(t,trapposition,f2,options2);
 b0 = c2.b0;
@@ -52,9 +52,18 @@ f2 = c2.f2;
 phi2 = c2.phi2;
 b = abs(b);
 phi2 = abs(phi2);
-trappositionfit = b0 + b.*sin(2*pi.*f2.*t + phi2);
+trappositionfit = b0 + b.*cos(2*pi.*f2.*t + phi2);
 
-phasediff = rad2deg(phi1-phi2);
+%Calculation of G' and G" at the given frequency. a = Ft0; b = Xt0;
+%r = bead radius; Kt = trap stiffness
+
+Ft0 = a;
+Xt0 = b/0.249; % Conversion factor for voltage to microns.
+Delta = phi1-phi2;
+Y = Ft0/(Xt0*Kt);
+
+G1 = (Ft0./(6*pi*r*Xt0)).*((cos(Delta)-Y)./((cos(Delta)-Y).^2+sin(Delta).^2));
+G2 = (Ft0./(6*pi*r*Xt0)).*sin(Delta)./((cos(Delta)-Y).^2+sin(Delta).^2);
 
 % Both the force signal response and the trap oscillation are plotted on
 % the same graph.
@@ -78,7 +87,7 @@ xlabel('Time (s)')
 ylabel('Trap position (\mum)')
 grid on
 
-% The output is further processed to obtain the complex moduli
-output = [num2str(a0),' ',num2str(a),' ',num2str(f1),' ',num2str(phi1),' ',num2str(b0),' ',num2str(b),' ',num2str(f2),' ',num2str(phi2),' ',num2str(phasediff)];
+% Output the complex moduli
+output = ['At an oscillation frequency of ',num2str(frequency),' Hz, the storage modulus is ',num2str(G1),' Pa, and the loss modulus is ',num2str(G2),' Pa'];
 
 end
